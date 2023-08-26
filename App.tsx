14622cd5c5063a896a11dc95dd1fc39ec7b97eca
bekirbali <bfbali43@gmail.com>
2023-08-26 23:45:57 +0300

    new data func

--- App.tsx
+++ App.tsx
@@ -31,6 +31,10 @@ export default function App() {
       });
   };
 
+  const getNewData = async () => {
+    console.log(city);
+  };
+
   const backGroundImage = {
     uri: "https://images.unsplash.com/photo-1567095761054-7a02e69e5c43?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80",
   };
